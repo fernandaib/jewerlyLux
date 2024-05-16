2bf0a51b92728e77d379cc8ebb27e4560b4349f4
Fernanda <107973615+fernandaib@users.noreply.github.com>
2024-05-15 21:34:02 -0700

    Add files via upload

---
+++ scripts/script.js
@@ -0,0 +1,39 @@
+// function myFunction() {    
+//     document.getElementById("aside-menu").classList.toggle("display-aside-menu");
+//     document.getElementById("open").classList.toggle("switchOff");
+//     document.getElementById("close").classList.toggle("switchOn");
+    
+//   }
+
+
+
+//Components: Hero Slides 
+
+const images = document.querySelectorAll('.jewerly-slideshow img');
+let currentIndex = 0;
+
+function showImage(index) {
+  images[currentIndex].classList.remove('active');
+  images[currentIndex].style.opacity = 0;
+  images[index].classList.add('active');
+  images[index].style.opacity = 1;
+  currentIndex = index;
+}
+
+images.forEach((image, index) => {
+  image.style.opacity = 0;
+  image.addEventListener('load', () => {
+    if(index === 0) {
+      image.classList.add('active');
+      image.style.opacity = 1;
+    }
+  })
+})
+
+setInterval(() => {
+  let nextIndex = currentIndex + 1;
+  if(nextIndex >= images.length) {
+    nextIndex = 0;
+  }
+  showImage(nextIndex)
+}, 3000);
